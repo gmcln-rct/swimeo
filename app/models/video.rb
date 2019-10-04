995bd712ee4609e67d703cada5416ebd9ec4d86a
Glenn McClanan <producersama@gmail.com>
2019-10-03 20:35:32 -0400

    Initiate AWS storage changes

---
+++ app/models/video.rb
@@ -0,0 +1,14 @@
+class Video < ApplicationRecord
+
+  has_one_attached :video_url
+
+
+  validates :title, :description, :creator_id, presence: true
+  validates :title, uniqueness: true
+
+  belongs_to :user,
+    foreign_key: :creator_id,
+    class_name: 'User'
+
+
+end
