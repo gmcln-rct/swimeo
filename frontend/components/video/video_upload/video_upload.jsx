f82d0b6ac22ee70645775ab2d8d47c6a908f9136
Glenn McClanan <producersama@gmail.com>
2019-10-05 22:12:39 -0400

    Fixes to video update page

--- frontend/components/video/video_upload/video_upload.jsx
+++ frontend/components/video/video_upload/video_upload.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 class VideoUpload extends React.Component {
     constructor(props) {
         super(props);
@@ -25,7 +27,6 @@ class VideoUpload extends React.Component {
         const file = e.currentTarget.files[0];
         const fileReader = new FileReader();
         fileReader.onloadend = () => {
-
             this.setState({ videoFile: file, video_url: fileReader.result });
         };
         if (file) {
@@ -65,53 +66,100 @@ class VideoUpload extends React.Component {
 
         return (
             <div className='video-upload-page-container'>
-                <section className='video-upload-container'>
-                    <form onSubmit={this.handleSubmit} className="video-upload-form">
+                <section className='video-upload-section'>
+                    <div className='video-upload-container'>
+                        <form onSubmit={this.handleSubmit} className="video-upload-form">
 
-                        <div className='upload-header-log'></div>
-                        <div className='upload-header'>Choose Video File to Upload</div>
-                        <input
-                            type="file"
-                            onChange={this.handleFile} />
+                            <div className='upload-header-logo'></div>
+                            <div className='upload-header'>Choose Video File to Upload</div>
                             <br/>
-                        <label>
-                            <div className='upload-input-label'>Title</div>
-                            
-                        <input 
-                            type="text"
-                            id="video-title"
-                            value={this.state.title}
-                            onChange={this.handleInput('title')} />
-                        </label>
 
-                        <label>
-                            <div className='upload-input-label'>Description</div>
+                            {/* <input
+                                type="file"
+                                onChange={this.handleFile} /> */}
 
-                            
-                        <textarea
-                                id="video-description"
-                                value={this.state.description}
-                                onChange={this.handleInput('description')} >
-                        </textarea>
-                        </label>
+                            <button className='upload-choose-file-button-faux'>
+                                {/* <span> <FontAwesomeIcon icon="cloud-upload-alt" /></span> */}
+                                <span> <FontAwesomeIcon icon="cloud-upload-alt" style={{ color: 'white' }} /></span>
+
+                                <span> Or choose files</span>
+                            </button>
+
+                            <br/>
+                            <label className='upload-input-label'>
+                                Title                         
+                                <input 
+                                    type="text"
+                                    className="video-title"
+                                    value={this.state.title}
+                                    className='upload-input-label'
+                                    placeholder="Enter video title"
+                                    title="Please enter a video title"
+                                />
+                            {/* <input 
+                                type="text"
+                                id="video-title"
+                                value={this.state.title}
+                                onChange={this.handleInput('title')} /> */}
+                            </label>
+                            <br/>
+                            <label className='upload-input-label'>
+                                Description
+                                <textarea
+                                        id="video-description"
+                                        value={this.state.description}
+                                        onChange={this.handleInput('description')} 
+                                        placeholder="Enter video description"
+                                        title="Please enter a video description."
+                                        
+                                        >
+                                </textarea>
+                            </label>
 
 
-                        {/* <h3>Thumbnail preview </h3> */}
-                        {/* {preview} */}
-                        <button onClick={this.props.upload}>Upload Video</button>
-                    </form>
-                </section>
-                <section className="video-upload-left-sidebar">
-                    <div className='video-upload-left-sidebar-1'> 
-                        <span>Weekly</span>
-                        <span>100TB</span>
+                            {/* <h3>Thumbnail preview </h3> */}
+                            {/* {preview} */}
+                            <button onClick={this.props.upload} className='upload-form-upload-button'>Upload Video</button>
+                        </form>
                     </div>
-                    <div className='video-upload-left-sidebar-1'>
-                        <span>Weekly</span>
-                        <span>100TB</span>
-                    </div>
-
+                    <div className='video-upload-form-footer'>Learn more about uploading in the Video Guidelines, Compression Guidelines, and Help Center.</div>
                 </section>
+
+                <section className="video-upload-sidebar">
+                    <div className='video-upload-sidebar-container'>
+                        <div className='video-upload-right-sidebar-headbar'></div>
+                        <div className='video-upload-right-sidebar-headbar-overlay'></div>
+                        <br/>
+                        <div className='video-upload-right-sidebar-1'> 
+                            <p>Weekly Limit</p>
+                            <p>100TB</p>
+                        </div>
+                        <br/>
+                        <div className='video-upload-right-sidebar-1'>
+                            <span>Total Limit</span>
+                            <span>100PB</span>
+                        </div>
+                        <br/>
+                        <div className='video-upload-right-sidebar-1'>
+                            <span>Upgrade for up to 1,000,000 PB</span>
+                        </div>
+                        <br/>
+                        <div className='video-upload-right-sidebar-subheader'>
+                            <span>Other ways to upload</span>
+                        </div>
+                        <br />
+
+
+                        <div className='video-upload-right-sidebar-buttons'>
+
+                            <button className='video-upload-fake-button'>DropBait</button>
+                            <button className='video-upload-fake-button'>GoogleDrop</button>
+                            <button className='video-upload-fake-button'>And More</button>
+
+                        </div>
+                    </div>
+                </section>
+
             </div>
         );
     }
