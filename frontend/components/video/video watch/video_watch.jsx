fff27cd95d80fe5f158e4f0c34dfbd5c01dc07a0
Glenn McClanan <producersama@gmail.com>
2019-10-07 23:03:25 -0400

    Build out watch page

--- frontend/components/video/video watch/video_watch.jsx
+++ frontend/components/video/video watch/video_watch.jsx
@@ -13,7 +13,7 @@ class VideoWatch extends React.Component {
 
     componentDidMount() {
         const id = this.props.match.params.id;
-        debugger
+        // debugger
         this.props.receiveVideos();
     }
 
@@ -26,28 +26,82 @@ class VideoWatch extends React.Component {
 
             <div className='watch-page-container'>
                 <section className='watch-page-sidebar'>
+
                     <Link to="/upload" className='nav-upload-button'>
                         Upload
                     </Link>
-                <div className='watch-sidebar-list'>
-                    <div className='watch-sidebar-list-item'>
-                        <FontAwesomeIcon icon='home' /> Home
-                    </div>
-                    <div className='watch-sidebar-list-item'>
-                            <FontAwesomeIcon icon='photo-video' /> Videos
-                    </div>
-                    {/* <span> Videos</span> */}
-                </div>
-                <div>
-                    Swimeo Pro
-                </div>
-                <div>
-                    Powerful privacy options and advanced stats.
-                </div>
 
+                    <div className='watch-sidebar-hr'>
+                        <hr/>
+                    </div>
+ 
+
+                    <div className='watch-sidebar-list'>
+
+                        <div className='watch-sidebar-list-item'>
+                            <svg viewBox="0 0 20 20">
+                                <path d="M17 20h-4c-.6 0-1-.4-1-1v-5H8v5c0 .6-.4 1-1 1H3c-1.1 0-2-.9-2-2V8c0-.3.1-.6.3-.8l8-7c.4-.3.9-.3 1.3 0l8 7c.3.2.4.5.4.8v10c0 1.1-.9 2-2 2zm-3-2h3V8.5l-7-6.1-7 6.1V18h3v-5c0-.6.4-1 1-1h6c.6 0 1 .4 1 1v5z"></path>
+                            </svg> 
+                                <div className='watch-sidebar-list-item-text'>
+                                    Home
+                                </div>
+                        </div>
+                        <div className='watch-sidebar-list-item'>
+                            <svg viewBox="0 0 20 20">
+                                <path d="M14 2v12H2V2h12m0-2H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" fill="#1a2e3b"></path>
+
+                                <path d="M5.6 5.51c0-.55.39-.77.86-.5l4.28 2.5a.54.54 0 0 1 0 1l-4.28 2.57c-.47.28-.86.07-.86-.49zM16 20a4 4 0 0 0 4-4V5a2 2 0 0 0-2-2v13a2 2 0 0 1-2 2H3a2 2 0 0 0 2 2z" fill="#192e3b" fill-rule="evenodd"></path>
+                            </svg>  
+                            <div className='watch-sidebar-list-item-text'>
+                                Videos
+                            </div>
+                        </div>
+                        {/* <span> Videos</span> */}
+                    </div>
+                    <div className='watch-sidebar-footer'>
+                        <h1>Swimeo Pro</h1>
+                        <h3>Powerful privacy options and advanced stats.</h3>
+                            <button className='splash-buttons-plan'>Upgrade</button>
+                    </div>
+                </section>
+
+                <section className='watch-page-main'>
+                    <div className='watch-page-main-header'>Videos</div>
+                    <div className='watch-page-main-grid'>
+
+                        <div className='watch-page-main-grid-item'>
+                            <img src="https://swimeo-seeds.s3.amazonaws.com/images/1-waterfall.jpg"  className='watch-page-thumb' alt="Video" />
+                            Video Thumbnail
+                        </div>
+                        <div className='watch-page-main-grid-item'>
+                            <img src="https://swimeo-seeds.s3.amazonaws.com/images/2-water-drops.jpg" className="watch-page-thumb" alt="Video" />
+                            Video Thumbnail
+                        </div>
+                        <div className='watch-page-main-grid-item'>
+                            <img src="https://swimeo-seeds.s3.amazonaws.com/images/3-tide.jpg" className="watch-page-thumb" alt="Video" />
+                            Video Thumbnail
+                        </div>
+                        <div className='watch-page-main-grid-item'>
+                            <img src="https://swimeo-seeds.s3.amazonaws.com/images/4-ocean-surface.jpg" className="watch-page-thumb" alt="Video" /> 
+                            Video Thumbnail
+                        </div>
+                        <div className='watch-page-main-grid-item'>
+                            <img src="https://swimeo-seeds.s3.amazonaws.com/images/2-water-drops.jpg" className="watch-page-thumb" alt="Video" />
+                            Video Thumbnail
+                        </div>
+                        <div className='watch-page-main-grid-item'>
+                            <img src="https://swimeo-seeds.s3.amazonaws.com/images/2-water-drops.jpg" className="watch-page-thumb" alt="Video" />
+                            Video Thumbnail
+                        </div>
+                        <div className='watch-page-main-grid-item'>
+                            <img src="https://swimeo-seeds.s3.amazonaws.com/images/2-water-drops.jpg" className="watch-page-thumb" alt="Video" />
+                            Video Thumbnail
+                        </div>
+
+
+                    </div>
 
                 </section>
-                <section className='watch-page-main'></section>
 
             </div>
             
