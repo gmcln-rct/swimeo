ebcc637c9ae2b3bb3ea6bb299401401085be0c43
Glenn McClanan <producersama@gmail.com>
2019-10-30 22:09:48 -0400

    Fix Video Show player border issue

--- frontend/components/video/video_show/video_show.jsx
+++ frontend/components/video/video_show/video_show.jsx
@@ -33,9 +33,7 @@ class VideoShow extends React.Component {
             <div className='video-show-page'>
 
                 <div className='video-unit-wrapper'>
-                    <div className='video-player-wrapper'>
                         <div className='video-container'>
-                            {/* <span>swimeo</span> */}
 
                             <video
                                 controls
@@ -43,70 +41,65 @@ class VideoShow extends React.Component {
                                 src={this.props.video.video_url} 
                                 type='video/webm' />
                         </div>
-                            <br/>
+                </div>
 
-                        <div className='video-footer'>
-                            <div className='video-footer-container'>
+                    <div className='video-footer'>
+                        <div className='video-footer-container'>
 
-                                <div className='video-info'>
+                            <div className='video-info'>
 
-                                    <div className='video-title'>{this.props.video.title}</div>
-                                    <br />
-                                    <div className='video-description'>
-                                        {this.props.video.description}
-                                    </div>
-                                    <div>
-                                        <VideoLikeComponent />
-                                    </div>
-                                    <br/>
-                                    <div>
-                                        <CommentIndexComponent />
-                                    </div>
+                                <div className='video-title'>{this.props.video.title}</div>
+                                <br />
+                                <div className='video-description'>
+                                    {this.props.video.description}
                                 </div>
-
-                                <div className='video-playlist'>
-                                    <div className='video-playlist-item'>
-                                        <span><Link to="/watch/5" ><img src="https://swimeo-seeds.s3.amazonaws.com/images/5-bubbles.jpg" className="playlist-thumb-1" alt="Video" /></Link></span>
-                                        <span>
-                                            <h1>Bubbles</h1>
-                                        </span>
-                                    </div>
-                                    <div className='video-playlist-item'>
-                                        <span><Link to="/watch/16" ><img src="https://swimeo-seeds.s3.amazonaws.com/images/16-kids-pool.jpg" className="playlist-thumb" alt="Video" /></Link></span>
-                                        <span>
-                                            <h1>Summer pool time</h1>
-                                        </span>
-                                    </div>
-                                    <div className='video-playlist-item'>
-                                        <span><Link to="/watch/11" ><img src="https://swimeo-seeds.s3.amazonaws.com/images/11-pool.jpg" className="playlist-thumb" alt="Video" /></Link></span>
-                                        <span>
-                                            <h1>tranquil pool water</h1>
-                                        </span>
-                                    </div>
-                                    <div className='video-playlist-item'>
-                                        <span><Link to="/watch/10" ><img src="https://swimeo-seeds.s3.amazonaws.com/images/10-michael-phelps.jpg" className="playlist-thumb" alt="Video" /></Link></span>
-                                        <span>
-                                            <h1>Michael Phelps swims!</h1>
-                                        </span>
-                                    </div>
-                                    <div className='video-playlist-item'>
-                                        <span><Link to="/watch/12" ><img src="https://swimeo-seeds.s3.amazonaws.com/images/12-rain-street.jpg" className="playlist-thumb" alt="Video" /></Link></span>
-                                        <span>
-                                            <h1>Rainy Streets</h1>
-                                        </span>
-                                    </div>
+                                <div>
+                                    <VideoLikeComponent />
                                 </div>
+                                <br/>
+                                <div>
+                                    <CommentIndexComponent />
+                                </div>
+                            </div>
 
+                            <div className='video-playlist'>
+                                <div className='video-playlist-item'>
+                                    <span><Link to="/watch/5" ><img src="https://swimeo-seeds.s3.amazonaws.com/images/5-bubbles.jpg" className="playlist-thumb-1" alt="Video" /></Link></span>
+                                    <span>
+                                        <h1>Bubbles</h1>
+                                    </span>
+                                </div>
+                                <div className='video-playlist-item'>
+                                    <span><Link to="/watch/16" ><img src="https://swimeo-seeds.s3.amazonaws.com/images/16-kids-pool.jpg" className="playlist-thumb" alt="Video" /></Link></span>
+                                    <span>
+                                        <h1>Summer pool time</h1>
+                                    </span>
+                                </div>
+                                <div className='video-playlist-item'>
+                                    <span><Link to="/watch/11" ><img src="https://swimeo-seeds.s3.amazonaws.com/images/11-pool.jpg" className="playlist-thumb" alt="Video" /></Link></span>
+                                    <span>
+                                        <h1>tranquil pool water</h1>
+                                    </span>
+                                </div>
+                                <div className='video-playlist-item'>
+                                    <span><Link to="/watch/10" ><img src="https://swimeo-seeds.s3.amazonaws.com/images/10-michael-phelps.jpg" className="playlist-thumb" alt="Video" /></Link></span>
+                                    <span>
+                                        <h1>Michael Phelps swims!</h1>
+                                    </span>
+                                </div>
+                                <div className='video-playlist-item'>
+                                    <span><Link to="/watch/12" ><img src="https://swimeo-seeds.s3.amazonaws.com/images/12-rain-street.jpg" className="playlist-thumb" alt="Video" /></Link></span>
+                                    <span>
+                                        <h1>Rainy Streets</h1>
+                                    </span>
+                                </div>
                             </div>
 
                         </div>
+
                     </div>
 
                 </div>
-
-
-            </div>
-        
            
         );
     };
