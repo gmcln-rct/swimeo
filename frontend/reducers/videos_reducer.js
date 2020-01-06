5744e8a75c6258f1ccc1262bcbf73de5023d560e
Glenn McClanan <producersama@gmail.com>
2020-01-06 14:15:47 -0500

    Clean up reducers

--- frontend/reducers/videos_reducer.js
+++ frontend/reducers/videos_reducer.js
@@ -7,9 +7,7 @@ const videosReducer = (state = {}, action) => {
     Object.freeze(state);
 
     switch (action.type) {
-
         case RECEIVE_VIDEOS:
-            
             return merge({}, state, action.videos);
         case RECEIVE_VIDEO:
             const {video} = action.video;
